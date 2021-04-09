6ce7530e6a616de973a1b74ea0784239548d2a36
bnemerson <bnemerson@pitttechnology.com>
2021-04-09 10:25:34 -0500

    Updated footer and CTA

--- site_settings/models.py
+++ site_settings/models.py
@@ -28,6 +28,25 @@ class ContactSettings(BaseSetting):
 
 
 @register_setting
+class HoursSettings(BaseSetting):
+
+    hours = RichTextField(
+        blank=True,
+        null=True,
+        features=[],
+    )
+
+    panels = [
+        FieldPanel("hours")
+    ]
+
+    def save(self, *args, **kwargs):
+        key = make_template_fragment_key("footer_hours_settings")
+        cache.delete(key)
+        return super().save(*args, **kwargs)
+
+
+@register_setting
 class SocialMediaSettings(BaseSetting):
     facebook = models.URLField(
         blank=True,
@@ -53,3 +72,32 @@ class SocialMediaSettings(BaseSetting):
         FieldPanel("instagram"),
     ]
 
+
+@register_setting
+class FooterCTASettings(BaseSetting):
+
+    title = models.CharField(max_length=100)
+    subtitle = models.CharField(max_length=250)
+    button_text = models.CharField(max_length=25, default='Contact Us')
+    button_internal_page = models.ForeignKey(
+        'wagtailcore.Page',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+',
+        help_text='If an internal page is selected, it will be used before the external page.'
+    )
+    button_external_page = models.URLField(blank=True, help_text='If an internal page is selected, it will be used before the external page.')
+
+    panels = [
+        FieldPanel("title"),
+        FieldPanel("subtitle"),
+        FieldPanel("button_text"),
+        PageChooserPanel("button_internal_page"),
+        FieldPanel("button_external_page"),
+    ]
+
+    def save(self, *args, **kwargs):
+        key = make_template_fragment_key("footer_cta_settings")
+        cache.delete(key)
+        return super().save(*args, **kwargs)
